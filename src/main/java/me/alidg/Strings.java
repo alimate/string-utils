8acebde795528bb58e9b6cc4106bea4e1ab9d6cb
Ali Dehghani <ali.dehghani.g@gmail.com>
2017-04-26 22:49:42 +0430

    Added first commit
    Signed-off-by: Ali Dehghani <ali.dehghani.g@gmail.com>

---
+++ src/main/java/me/alidg/Strings.java
@@ -0,0 +1,10 @@
+package me.alidg;
+
+/**
+ * @author Ali Dehghani
+ */
+public class Strings {
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+}
